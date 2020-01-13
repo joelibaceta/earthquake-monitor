b41efda2aeb35ba08ce43ccf7ae15f8e8e16dd83
Joel Ibaceta <mail@joelibaceta.com>
2020-01-12 22:30:30 -0500

    Initial Commit

---
+++ earthquake-monitor WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  earthquake-monitor WatchKit Extension
+//
+//  Created by Joel Ibaceta on 1/12/20.
+//  Copyright © 2020 Joel Ibaceta. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
